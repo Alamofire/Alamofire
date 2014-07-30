c20b5602a3d58d1887f144aa12b0d95891ca1cea
Mattt Thompson <m@mattt.me>
2014-07-30 17:31:10 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Alamofire
+## Elegant Networking in Swift
+
+---
+
+## Contact
+
+Follow AFNetworking on Twitter ([@AFNetworking](https://twitter.com/AFNetworking))
+
+### Creator
+
+- [Mattt Thompson](http://github.com/mattt) ([@mattt](https://twitter.com/mattt))
+
+## License
+
+Alamofire is released under an MIT license. See LICENSE for more information.
---
+++ Tests/ResponseTests.swift
@@ -0,0 +1,50 @@
+// ResponseTests.swift
+//
+// Copyright (c) 2014 Alamofire (http://alamofire.org)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import Foundation
+import XCTest
+
+extension Alamofire {
+    struct ResponseTests {
+        class JSONResponseTestCase: XCTestCase {
+            func testJSONResponse() {
+                let URL = "http://httpbin.org/get"
+                let expectation = expectationWithDescription("\(URL)")
+
+                Alamofire.request(.GET, URL, parameters: ["foo": "bar"])
+                         .responseJSON { (request, response, JSON, error) in
+                            expectation.fulfill()
+                            XCTAssertNotNil(request, "request should not be nil")
+                            XCTAssertNotNil(response, "response should not be nil")
+                            XCTAssertNotNil(JSON, "JSON should not be nil")
+                            XCTAssertNil(error, "error should be nil")
+
+                            XCTAssertEqual(JSON!["args"] as NSObject, ["foo": "bar"], "args should be equal")
+                         }
+
+                waitForExpectationsWithTimeout(10){ error in
+                    XCTAssertNil(error, "\(error)")
+                }
+            }
+        }
+    }
+}
