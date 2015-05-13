b37e62246919646f20f9f92b7311c0532b1d02e9
Petr Korolev <sky4winder@gmail.com>
2015-05-13 19:23:49 +0300

    fix test closure

--- Tests/ParameterEncodingTests.swift
+++ Tests/ParameterEncodingTests.swift
@@ -341,7 +341,7 @@ class AlamofirePropertyListParameterEncodingTestCase: XCTestCase {
 
 class AlamofireCustomParameterEncodingTestCase: XCTestCase {
     func testCustomParameterEncode() {
-        let encodingClosure: (URLRequestConvertible, [String: AnyObject]?) -> (NSURLRequest, NSError?) = { URLRequest, parameters in
+        let encodingClosure: (URLRequestConvertible, [String: AnyObject]?) -> (NSMutableURLRequest, NSError?) = { URLRequest, parameters in
             let mutableURLRequest = URLRequest.URLRequest.mutableCopy() as! NSMutableURLRequest
             mutableURLRequest.setValue("Xcode", forHTTPHeaderField: "User-Agent")
             return (mutableURLRequest, nil)
