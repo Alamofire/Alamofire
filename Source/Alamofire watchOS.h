146297ef604e3c6e9a217ff7497ddaeb33a9114c
Tobias Ottenweller <tottenweller@babbel.com>
2015-07-27 17:47:35 +0200

    watchOS target added.

---
+++ Source/Alamofire watchOS.h
@@ -0,0 +1,31 @@
+// Alamofire.h
+//
+// Copyright (c) 2014–2015 Alamofire Software Foundation (http://alamofire.org/)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Swifty JSON watchOS.
+FOUNDATION_EXPORT double Alamofire_watchOSVersionNumber;
+
+//! Project version string for Swifty JSON watchOS.
+FOUNDATION_EXPORT const unsigned char Alamofire_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swifty_JSON_watchOS/PublicHeader.h>
