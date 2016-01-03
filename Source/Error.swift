c54aa1932d62c32b604fb26809fb7769b9ab5f00
Christian Noon <christian.noon@gmail.com>
2016-01-02 22:27:22 -0800

    Updated the copyright years for the license across all project files.

--- Source/Error.swift
+++ Source/Error.swift
@@ -1,6 +1,6 @@
 // Error.swift
 //
-// Copyright (c) 2014–2015 Alamofire Software Foundation (http://alamofire.org/)
+// Copyright (c) 2014–2016 Alamofire Software Foundation (http://alamofire.org/)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
