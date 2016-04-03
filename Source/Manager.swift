8ea36fccceddfd7bf26b5350a7a84b86acb70aae
Christian Noon <christian.noon@gmail.com>
2016-04-03 13:59:38 -0700

    Fixed issue where challenge and response closures with completion were not called.

--- Source/Manager.swift
+++ Source/Manager.swift
@@ -740,11 +740,11 @@ public class Manager {
             case #selector(NSURLSessionDelegate.URLSession(_:didBecomeInvalidWithError:)):
                 return sessionDidBecomeInvalidWithError != nil
             case #selector(NSURLSessionDelegate.URLSession(_:didReceiveChallenge:completionHandler:)):
-                return sessionDidReceiveChallenge != nil
+                return (sessionDidReceiveChallenge != nil  || sessionDidReceiveChallengeWithCompletion != nil)
             case #selector(NSURLSessionTaskDelegate.URLSession(_:task:willPerformHTTPRedirection:newRequest:completionHandler:)):
                 return (taskWillPerformHTTPRedirection != nil || taskWillPerformHTTPRedirectionWithCompletion != nil)
             case #selector(NSURLSessionDataDelegate.URLSession(_:dataTask:didReceiveResponse:completionHandler:)):
-                return dataTaskDidReceiveResponse != nil
+                return (dataTaskDidReceiveResponse != nil || dataTaskDidReceiveResponseWithCompletion != nil)
             default:
                 return self.dynamicType.instancesRespondToSelector(selector)
             }
