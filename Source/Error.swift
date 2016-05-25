469662c4a944dfc21bd3bcfbe37b24181e814bec
Juan Alvarez <jlalvarez18@users.noreply.github.com>
2016-05-25 17:14:05 -0400

    made public non-deprecated functions in Error

--- Source/Error.swift
+++ Source/Error.swift
@@ -77,11 +77,11 @@ public struct Error {
         return NSError(domain: Domain, code: code, userInfo: userInfo)
     }
 
-    static func error(domain domain: String = Error.Domain, code: Code, failureReason: String) -> NSError {
+    public static func error(domain domain: String = Error.Domain, code: Code, failureReason: String) -> NSError {
         return error(domain: domain, code: code.rawValue, failureReason: failureReason)
     }
 
-    static func error(domain domain: String = Error.Domain, code: Int, failureReason: String) -> NSError {
+    public static func error(domain domain: String = Error.Domain, code: Int, failureReason: String) -> NSError {
         let userInfo = [NSLocalizedFailureReasonErrorKey: failureReason]
         return NSError(domain: domain, code: code, userInfo: userInfo)
     }
