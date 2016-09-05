2b4c443faafe119225521dd2770d25ce783c82d9
Christian Noon <christian.noon@gmail.com>
2016-09-04 17:36:26 -0700

    BaseTestCase now deletes contents of common directories using FileManager extension.

--- Tests/BaseTestCase.swift
+++ Tests/BaseTestCase.swift
@@ -29,6 +29,14 @@ import XCTest
 class BaseTestCase: XCTestCase {
     let timeout: TimeInterval = 30.0
 
+    override func setUp() {
+        super.setUp()
+
+        FileManager.removeAllItemsInsideDirectory(atPath: FileManager.applicationSupportDirectory)
+        FileManager.removeAllItemsInsideDirectory(atPath: FileManager.cachesDirectory)
+        FileManager.removeAllItemsInsideDirectory(atPath: FileManager.documentsDirectory)
+    }
+
     func url(forResource fileName: String, withExtension ext: String) -> URL {
         let bundle = Bundle(for: BaseTestCase.self)
         return bundle.url(forResource: fileName, withExtension: ext)!
