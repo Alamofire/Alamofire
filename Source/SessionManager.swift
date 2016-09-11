e50160acda93a4309dc862fcbec32602cf805528
Christian Noon <christian.noon@gmail.com>
2016-09-11 04:00:48 -0700

    Replaced instances of `com.alamofire` with `org.alamofire` throughout project.

--- Source/SessionManager.swift
+++ Source/SessionManager.swift
@@ -64,7 +64,7 @@ open class SessionManager {
         }.joined(separator: ", ")
 
         // User-Agent Header; see https://tools.ietf.org/html/rfc7231#section-5.5.3
-        // Example: `iOS Example/1.0 (com.alamofire.iOS-Example; build:1; iOS 9.3.0) Alamofire/3.4.2`
+        // Example: `iOS Example/1.0 (org.alamofire.iOS-Example; build:1; iOS 10.0.0) Alamofire/4.0.0`
         let userAgent: String = {
             if let info = Bundle.main.infoDictionary {
                 let executable = info[kCFBundleExecutableKey as String] as? String ?? "Unknown"
