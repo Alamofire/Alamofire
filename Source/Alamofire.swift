4af61a695b551b81b7ee7e3473f8017b38df3e61
Christian Noon <christian.noon@gmail.com>
2016-10-01 23:22:33 -0700

    Added docstrings and note to README about resumeData and background sessions.

--- Source/Alamofire.swift
+++ Source/Alamofire.swift
@@ -222,6 +222,13 @@ public func download(
 /// If `destination` is not specified, the contents will remain in the temporary location determined by the
 /// underlying URL session.
 ///
+/// On the latest release of all the Apple platforms (iOS 10, macOS 10.12, tvOS 10, watchOS 3), `resumeData` is broken 
+/// on background URL session configurations. There's an underlying bug in the `resumeData` generation logic where the 
+/// data is written incorrectly and will always fail to resume the download. For more information about the bug and 
+/// possible workarounds, please refer to the following Stack Overflow post:
+///
+///    - http://stackoverflow.com/a/39347461/1342462
+///
 /// - parameter resumeData:  The resume data. This is an opaque data blob produced by `URLSessionDownloadTask`
 ///                          when a task is cancelled. See `URLSession -downloadTask(withResumeData:)` for additional
 ///                          information.
--- Source/SessionManager.swift
+++ Source/SessionManager.swift
@@ -348,6 +348,13 @@ open class SessionManager {
     ///
     /// If `startRequestsImmediately` is `true`, the request will have `resume()` called before being returned.
     ///
+    /// On the latest release of all the Apple platforms (iOS 10, macOS 10.12, tvOS 10, watchOS 3), `resumeData` is broken
+    /// on background URL session configurations. There's an underlying bug in the `resumeData` generation logic where the
+    /// data is written incorrectly and will always fail to resume the download. For more information about the bug and
+    /// possible workarounds, please refer to the following Stack Overflow post:
+    ///
+    ///    - http://stackoverflow.com/a/39347461/1342462
+    ///
     /// - parameter resumeData:  The resume data. This is an opaque data blob produced by `URLSessionDownloadTask`
     ///                          when a task is cancelled. See `URLSession -downloadTask(withResumeData:)` for
     ///                          additional information.
