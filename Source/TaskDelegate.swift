fcbfb3c9a8a2b416f3dfb30d3da386538aaa39d3
Jon Shier <jon@jonshier.com>
2017-07-02 21:06:43 -0400

    Add an NSLock for the task delegate’s task.

--- Source/TaskDelegate.swift
+++ Source/TaskDelegate.swift
@@ -38,11 +38,22 @@ open class TaskDelegate: NSObject {
 
     /// The error generated throughout the lifecyle of the task.
     public var error: Error?
-
-    var task: URLSessionTask? {
+    
+    private var _task: URLSessionTask? {
         didSet { reset() }
     }
-
+    var task: URLSessionTask? {
+        set {
+            taskLock.lock(); defer { taskLock.unlock() }
+            _task = newValue
+        }
+        get {
+            taskLock.lock(); defer { taskLock.unlock() }
+            return _task
+        }
+    }
+    
+    private let taskLock = NSLock()
     var initialResponseTime: CFAbsoluteTime?
     var credential: URLCredential?
     var metrics: AnyObject? // URLSessionTaskMetrics
@@ -50,7 +61,7 @@ open class TaskDelegate: NSObject {
     // MARK: Lifecycle
 
     init(task: URLSessionTask?) {
-        self.task = task
+        _task = task
 
         self.queue = {
             let operationQueue = OperationQueue()
