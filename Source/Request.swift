e0cb56a476c53ecf4ef021004e5241060f070735
Christian Noon <christian.noon@gmail.com>
2017-09-05 21:39:48 -0700

    Fixed #if swift formatting throughout codebase…no functional changes.

--- Source/Request.swift
+++ Source/Request.swift
@@ -309,11 +309,12 @@ extension Request: CustomDebugStringConvertible {
                 let cookies = cookieStorage.cookies(for: url), !cookies.isEmpty
             {
                 let string = cookies.reduce("") { $0 + "\($1.name)=\($1.value);" }
-                #if swift(>=3.2)
+
+            #if swift(>=3.2)
                 components.append("-b \"\(string[..<string.index(before: string.endIndex)])\"")
-                #else
+            #else
                 components.append("-b \"\(string.substring(to: string.characters.index(before: string.endIndex)))\"")
-                #endif
+            #endif
             }
         }
 
