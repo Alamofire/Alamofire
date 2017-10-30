ea082ceb5f5801baeda58c4e8abb0a3625e494d5
Jon Shier <jon@jonshier.com>
2017-10-30 18:42:37 -0400

    Add brotli support to default Accept-Encoding header.

--- Source/SessionManager.swift
+++ Source/SessionManager.swift
@@ -55,7 +55,7 @@ open class SessionManager {
     /// Creates default values for the "Accept-Encoding", "Accept-Language" and "User-Agent" headers.
     open static let defaultHTTPHeaders: HTTPHeaders = {
         // Accept-Encoding HTTP Header; see https://tools.ietf.org/html/rfc7230#section-4.2.3
-        let acceptEncoding: String = "gzip;q=1.0, compress;q=0.5"
+        let acceptEncoding: String = "br;q=1.0, gzip;q=0.9, compress;q=0.8"
 
         // Accept-Language HTTP Header; see https://tools.ietf.org/html/rfc7231#section-5.3.5
         let acceptLanguage = Locale.preferredLanguages.prefix(6).enumerated().map { index, languageCode in
