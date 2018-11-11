62603e6ad93d9cf520215c48b1994de53b1962b7
Jon Shier <jon@jonshier.com>
2018-11-11 02:18:00 -0500

    Sort HTTPHeader methods.

--- Source/HTTPMethod.swift
+++ Source/HTTPMethod.swift
@@ -26,13 +26,13 @@
 ///
 /// See https://tools.ietf.org/html/rfc7231#section-4.3
 public enum HTTPMethod: String {
-    case options = "OPTIONS"
+    case connect = "CONNECT"
+    case delete  = "DELETE"
     case get     = "GET"
     case head    = "HEAD"
+    case options = "OPTIONS"
+    case patch   = "PATCH"
     case post    = "POST"
     case put     = "PUT"
-    case patch   = "PATCH"
-    case delete  = "DELETE"
     case trace   = "TRACE"
-    case connect = "CONNECT"
 }
