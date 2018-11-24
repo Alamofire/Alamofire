f2658461df1014ae80da8586169f84903022074e
Christian Noon <christian.noon@gmail.com>
2018-11-24 14:38:30 -0800

    Alamofire 5: Empty Response Refactor (2nd Refactor) (#2646)
    * Removed AnyResponseSerializer since it is no longer in use
    
    * Moved empty request and response status code logic into protocol extension

--- Source/ResponseSerialization.swift
+++ Source/ResponseSerialization.swift
@@ -45,11 +45,31 @@ public protocol DownloadResponseSerializerProtocol {
 }
 
 /// A serializer that can handle both data and download responses.
-public protocol ResponseSerializer: DataResponseSerializerProtocol & DownloadResponseSerializerProtocol { }
+public protocol ResponseSerializer: DataResponseSerializerProtocol & DownloadResponseSerializerProtocol {
+    static var defaultEmptyRequestMethods: Set<HTTPMethod> { get }
+    static var defaultEmptyResponseCodes: Set<Int> { get }
+
+    var emptyRequestMethods: Set<HTTPMethod> { get }
+    var emptyResponseCodes: Set<Int> { get }
+}
 
 extension ResponseSerializer {
-    public static var defaultEmptyResponseCodes: Set<Int> { return emptyResponseCodes }
-    public static var defaultEmptyRequestMethods: Set<HTTPMethod> { return emptyRequestMethods }
+    public static var defaultEmptyRequestMethods: Set<HTTPMethod> { return [.head] }
+    public static var defaultEmptyResponseCodes: Set<Int> { return [204, 205] }
+
+    var emptyRequestMethods: Set<HTTPMethod> { return Self.defaultEmptyRequestMethods }
+    var emptyResponseCodes: Set<Int> { return Self.defaultEmptyResponseCodes }
+
+    public func requestAllowsEmptyResponseData(_ request: URLRequest?) -> Bool? {
+        return request.flatMap { $0.httpMethod }
+                      .flatMap(HTTPMethod.init)
+                      .map { emptyRequestMethods.contains($0) }
+    }
+
+    public func responseAllowsEmptyResponseData(_ response: HTTPURLResponse?) -> Bool? {
+        return response.flatMap { $0.statusCode }
+                       .map { emptyResponseCodes.contains($0) }
+    }
 }
 
 /// By default, any serializer declared to conform to both types will get file serialization for free, as it just feeds
@@ -74,66 +94,6 @@ public extension DownloadResponseSerializerProtocol where Self: DataResponseSeri
         } catch {
             throw error
         }
-    }
-}
-
-// MARK: - AnyResponseSerializer
-
-/// A generic `ResponseSerializer` conforming type.
-public final class AnyResponseSerializer<Value>: ResponseSerializer {
-    /// A closure which can be used to serialize data responses.
-    public typealias DataSerializer = (_ request: URLRequest?, _ response: HTTPURLResponse?, _ data: Data?, _ error: Error?) throws -> Value
-    /// A closure which can be used to serialize download reponses.
-    public typealias DownloadSerializer = (_ request: URLRequest?, _ response: HTTPURLResponse?, _ fileURL: URL?, _ error: Error?) throws -> Value
-
-    let dataSerializer: DataSerializer
-    let downloadSerializer: DownloadSerializer?
-
-    /// Initialze the instance with both a `DataSerializer` closure and a `DownloadSerializer` closure.
-    ///
-    /// - Parameters:
-    ///   - dataSerializer:     A `DataSerializer` closure.
-    ///   - downloadSerializer: A `DownloadSerializer` closure.
-    public init(dataSerializer: @escaping DataSerializer, downloadSerializer: @escaping DownloadSerializer) {
-        self.dataSerializer = dataSerializer
-        self.downloadSerializer = downloadSerializer
-    }
-
-    /// Initialze the instance with a `DataSerializer` closure. Download serialization will fallback to a default
-    /// implementation.
-    ///
-    /// - Parameters:
-    ///   - dataSerializer:     A `DataSerializer` closure.
-    public init(dataSerializer: @escaping DataSerializer) {
-        self.dataSerializer = dataSerializer
-        self.downloadSerializer = nil
-    }
-
-    public func serialize(request: URLRequest?, response: HTTPURLResponse?, data: Data?, error: Error?) throws -> Value {
-        return try dataSerializer(request, response, data, error)
-    }
-
-    public func serializeDownload(request: URLRequest?, response: HTTPURLResponse?, fileURL: URL?, error: Error?) throws -> Value {
-        return try downloadSerializer?(request, response, fileURL, error) ?? { (request, response, fileURL, error) in
-            guard error == nil else { throw error! }
-
-            guard let fileURL = fileURL else {
-                throw AFError.responseSerializationFailed(reason: .inputFileNil)
-            }
-
-            let data: Data
-            do {
-                data = try Data(contentsOf: fileURL)
-            } catch {
-                throw AFError.responseSerializationFailed(reason: .inputFileReadFailed(at: fileURL))
-            }
-
-            do {
-                return try serialize(request: request, response: response, data: data, error: error)
-            } catch {
-                throw error
-            }
-        }(request, response, fileURL, error)
     }
 }
 
@@ -329,12 +289,7 @@ public final class DataResponseSerializer: ResponseSerializer {
         guard error == nil else { throw error! }
         
         guard let data = data, !data.isEmpty else {
-            let requestAllowsEmptyResponses = request.flatMap { $0.httpMethod }
-                                                     .flatMap(HTTPMethod.init)
-                                                     .map { emptyRequestMethods.contains($0) }
-            let responseAllowsEmptyResponses = response.flatMap { $0.statusCode }
-                                                       .map { emptyResponseCodes.contains($0) }
-            if requestAllowsEmptyResponses ?? responseAllowsEmptyResponses ?? false {
+            if requestAllowsEmptyResponseData(request) ?? responseAllowsEmptyResponseData(response) ?? false {
                 return Data()
             } else {
                 throw AFError.responseSerializationFailed(reason: .inputDataNilOrZeroLength)
@@ -400,12 +355,7 @@ public final class StringResponseSerializer: ResponseSerializer {
         guard error == nil else { throw error! }
 
         guard let data = data else {
-            let requestAllowsEmptyResponses = request.flatMap { $0.httpMethod }
-                                                     .flatMap(HTTPMethod.init)
-                                                     .map { emptyRequestMethods.contains($0) }
-            let responseAllowsEmptyResponses = response.flatMap { $0.statusCode }
-                                                       .map { emptyResponseCodes.contains($0) }
-            if requestAllowsEmptyResponses ?? responseAllowsEmptyResponses ?? false {
+            if requestAllowsEmptyResponseData(request) ?? responseAllowsEmptyResponseData(response) ?? false {
                 return ""
             } else {
                 throw AFError.responseSerializationFailed(reason: .inputDataNilOrZeroLength)
@@ -507,12 +457,7 @@ public final class JSONResponseSerializer: ResponseSerializer {
         guard error == nil else { throw error! }
 
         guard let data = data, !data.isEmpty else {
-            let requestAllowsEmptyResponses = request.flatMap { $0.httpMethod }
-                                                     .flatMap(HTTPMethod.init)
-                                                     .map { emptyRequestMethods.contains($0) }
-            let responseAllowsEmptyResponses = response.flatMap { $0.statusCode }
-                                                       .map { emptyResponseCodes.contains($0) }
-            if requestAllowsEmptyResponses ?? responseAllowsEmptyResponses ?? false {
+            if requestAllowsEmptyResponseData(request) ?? responseAllowsEmptyResponseData(response) ?? false {
                 return NSNull()
             } else {
                 throw AFError.responseSerializationFailed(reason: .inputDataNilOrZeroLength)
@@ -607,16 +552,11 @@ public final class JSONDecodableResponseSerializer<T: Decodable>: ResponseSerial
         guard error == nil else { throw error! }
 
         guard let data = data, !data.isEmpty else {
-            let requestAllowsEmptyResponses = request.flatMap { $0.httpMethod }
-                                                     .flatMap(HTTPMethod.init)
-                                                     .map { emptyRequestMethods.contains($0) }
-            let responseAllowsEmptyResponses = response.flatMap { $0.statusCode }
-                                                       .map { emptyResponseCodes.contains($0) }
-            if requestAllowsEmptyResponses ?? responseAllowsEmptyResponses ?? false {
+            if requestAllowsEmptyResponseData(request) ?? responseAllowsEmptyResponseData(response) ?? false {
                 guard let emptyValue = Empty.value as? T else {
                     throw AFError.responseSerializationFailed(reason: .invalidEmptyResponse(type: "\(T.self)"))
                 }
-                
+
                 return emptyValue
             } else {
                 throw AFError.responseSerializationFailed(reason: .inputDataNilOrZeroLength)
@@ -650,7 +590,3 @@ extension DataRequest {
                         completionHandler: completionHandler)
     }
 }
-
-/// A set of HTTP response status code that do not contain response data.
-private let emptyResponseCodes: Set<Int> = [204, 205]
-private let emptyRequestMethods: Set<HTTPMethod> = [.head]
