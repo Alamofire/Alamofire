13b3e10b002489f807071d1d4973065aa1fb9888
Christian Noon <christian.noon@gmail.com>
2019-01-15 11:32:14 -0800

    Refactored copyright headers to only include 2014 creation date

--- Source/Validation.swift
+++ Source/Validation.swift
@@ -1,7 +1,7 @@
 //
 //  Validation.swift
 //
-//  Copyright (c) 2014-2018 Alamofire Software Foundation (http://alamofire.org/)
+//  Copyright (c) 2014 Alamofire Software Foundation (http://alamofire.org/)
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
