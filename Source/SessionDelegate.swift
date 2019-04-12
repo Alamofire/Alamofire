d24da7777c8801ed46cf34e03ab753068b4709ad
Christian Noon <christian.noon@gmail.com>
2019-04-12 16:39:50 -0700

    Audit - SessionStateProvider ACL (#2801)
    * Converted the SessionStateProvider protocol to internal type
    
    * Renamed the SessionStateProvider file to SessionDelegate

--- Source/SessionDelegate.swift
+++ Source/SessionDelegate.swift
@@ -1,5 +1,5 @@
 //
-//  SessionStateProvider.swift
+//  SessionDelegate.swift
 //
 //  Copyright (c) 2014-2018 Alamofire Software Foundation (http://alamofire.org/)
 //
@@ -24,7 +24,7 @@
 
 import Foundation
 
-public protocol SessionStateProvider: AnyObject {
+protocol SessionStateProvider: AnyObject {
     var serverTrustManager: ServerTrustManager? { get }
     var redirectHandler: RedirectHandler? { get }
     var cachedResponseHandler: CachedResponseHandler? { get }
