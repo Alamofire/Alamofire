d67ebd5deb9239a97fd2d3dd7ab820708020bc84
Jon Shier <jon@jonshier.com>
2019-06-13 22:04:35 -0400

    Continue Session docs.

--- Source/Session.swift
+++ Source/Session.swift
@@ -36,17 +36,17 @@ open class Session {
     public let session: URLSession
     /// Instance's `SessionDelegate`, which handles the `URLSessionDelegate` methods and `Request` interaction.
     public let delegate: SessionDelegate
-    /// Root queue for all internal callbacks and state update. **MUST** be a serial queue.
+    /// Root `DispatchQueue` for all internal callbacks and state update. **MUST** be a serial queue.
     public let rootQueue: DispatchQueue
     /// Value determining whether this instance automatically calls `resume()` on all created `Request`s.
     public let startRequestsImmediately: Bool
-    /// Queue on which `URLRequest`s are created asynchronously. By default this queue uses `rootQueue` as its `target`,
-    /// but a separate queue can be used if request creation is determined to be a bottleneck. Always profile and test
-    /// before introduing an additional queue.
+    /// `DispatchQueue` on which `URLRequest`s are created asynchronously. By default this queue uses `rootQueue` as its
+    /// `target`, but a separate queue can be used if request creation is determined to be a bottleneck. Always profile
+    /// and test before introduing an additional queue.
     public let requestQueue: DispatchQueue
-    /// Queue passed to all `Request`s on which they perform their response serialization. By default this queue uses
-    /// `rootQueue` as its `target` but a separate queue can be used if response serialization is determined to be a
-    /// bottleneck. Always profile and test before introducing an additional queue.
+    /// `DispatchQueue` passed to all `Request`s on which they perform their response serialization. By default this
+    /// queue uses `rootQueue` as its `target` but a separate queue can be used if response serialization is determined
+    /// to be a bottleneck. Always profile and test before introducing an additional queue.
     public let serializationQueue: DispatchQueue
     /// `RequestInterceptor` used for all `Request` created by the instance. `RequestInterceptor`s can also be set on a
     /// per-`Request` basis, in which case the `Request`'s interceptor takes precedence over this value.
@@ -61,11 +61,38 @@ open class Session {
     public let eventMonitor: CompositeEventMonitor
     /// `EventMonitor`s included in all instances. Defaults to `[AlamofireNotifications()]`.
     public let defaultEventMonitors: [EventMonitor] = [AlamofireNotifications()]
-    
 
     /// Internal map between `Request`s and any `URLSessionTasks` that may be in flight for them.
     var requestTaskMap = RequestTaskMap()
 
+    /// Create a `Session` from a `URLSession` and other parameters.
+    ///
+    /// - Note: If you create a `delegateQueue` for the passed `URLSession`, you must also use that queue's
+    ///         `underlyingQueue` as the `rootQueue` of this instance.
+    ///
+    /// - Parameters:
+    ///   - session:                  Underlying `URLSession` for this instance.
+    ///   - delegate:                 `SessionDelegate` that handles `session`'s delegate callbacks as well as `Request`
+    ///                               interaction.
+    ///   - rootQueue:                Root `DispatchQueue` for all internal callbacks and state updats. **MUST** be a
+    ///                               serial queue.
+    ///   - startRequestsImmediately: Determines whether this instance will automatically start all `Request`s. Defaults
+    ///                               to `true`. If set to `false`, all `Request`s created must have `.resume()` called
+    ///                               on them for them to start.
+    ///   - requestQueue:             `DispatchQueue` on which to perform `URLRequest` creation. By default this queue
+    ///                               will use the `rootQueue` as its `target`. A separate queue can be used if it's
+    ///                               determined request creation is a bottleneck, but that should only be done after
+    ///                               careful testing and profiling.
+    ///   - serializationQueue:       `DispatchQueue` on which to perform all response serialization. By default this
+    ///                               queue will use the `rootQueue` as its `target`. A separate queue can be used if
+    ///                               it's determined response serialization is a bottleneck, but that should only be
+    ///                               done after careful testing and profiling.
+    ///   - interceptor:              `RequestInterceptor` to be used for all `Request`s created by this instance.
+    ///   - serverTrustManager:       `ServerTrustManager` to be used for all trust evaluations by this instance.
+    ///   - redirectHandler:          `RedirectHandler` to be used by all `Request`s created by this instance.
+    ///   - cachedResponseHandler:    `CachedResponseHandler` to be used by all `Request`s created by this instance.
+    ///   - eventMonitors:            Additional `EventMonitor`s used by the instance. Alamofire always adds a
+    ///                               `AlamofireNotifications` `EventMonitor` to the array passed here.
     public init(session: URLSession,
                 delegate: SessionDelegate,
                 rootQueue: DispatchQueue,
@@ -78,7 +105,7 @@ open class Session {
                 cachedResponseHandler: CachedResponseHandler? = nil,
                 eventMonitors: [EventMonitor] = []) {
         precondition(session.delegateQueue.underlyingQueue === rootQueue,
-                     "SessionManager(session:) intializer must be passed the DispatchQueue used as the delegateQueue's underlyingQueue as rootQueue.")
+                     "Session(session:) intializer must be passed the DispatchQueue used as the delegateQueue's underlyingQueue as rootQueue.")
 
         self.session = session
         self.delegate = delegate
@@ -95,6 +122,33 @@ open class Session {
         delegate.stateProvider = self
     }
 
+    /// Create a `Session` from a `URLSessionConfiguration`. This intializer lets Alamofire handle the creation of the
+    /// underlying `URLSession` and its `delegateQueue`, and is the recommended intiailizer for most uses.
+    ///
+    /// - Parameters:
+    ///   - configuration:            `URLSessionConfiguration` to be used to create the underlying `URLSession`. Changes
+    ///                               to this value after being passed to this initializer will have no effect.
+    ///   - delegate:                 `SessionDelegate` that handles `session`'s delegate callbacks as well as `Request`
+    ///                               interaction.
+    ///   - rootQueue:                Root `DispatchQueue` for all internal callbacks and state updats. **MUST** be a
+    ///                               serial queue.
+    ///   - startRequestsImmediately: Determines whether this instance will automatically start all `Request`s. Defaults
+    ///                               to `true`. If set to `false`, all `Request`s created must have `.resume()` called
+    ///                               on them for them to start.
+    ///   - requestQueue:             `DispatchQueue` on which to perform `URLRequest` creation. By default this queue
+    ///                               will use the `rootQueue` as its `target`. A separate queue can be used if it's
+    ///                               determined request creation is a bottleneck, but that should only be done after
+    ///                               careful testing and profiling.
+    ///   - serializationQueue:       `DispatchQueue` on which to perform all response serialization. By default this
+    ///                               queue will use the `rootQueue` as its `target`. A separate queue can be used if
+    ///                               it's determined response serialization is a bottleneck, but that should only be
+    ///                               done after careful testing and profiling.
+    ///   - interceptor:              `RequestInterceptor` to be used for all `Request`s created by this instance.
+    ///   - serverTrustManager:       `ServerTrustManager` to be used for all trust evaluations by this instance.
+    ///   - redirectHandler:          `RedirectHandler` to be used by all `Request`s created by this instance.
+    ///   - cachedResponseHandler:    `CachedResponseHandler` to be used by all `Request`s created by this instance.
+    ///   - eventMonitors:            Additional `EventMonitor`s used by the instance. Alamofire always adds a
+    ///                               `AlamofireNotifications` `EventMonitor` to the array passed here.
     public convenience init(configuration: URLSessionConfiguration = URLSessionConfiguration.af.default,
                             delegate: SessionDelegate = SessionDelegate(),
                             rootQueue: DispatchQueue = DispatchQueue(label: "org.alamofire.session.rootQueue"),
@@ -142,6 +196,16 @@ open class Session {
         }
     }
 
+    /// Create a `DataRequest` from a `URLRequest` created using the passeed components and a `RequestInterceptor`.
+    ///
+    /// - Parameters:
+    ///   - url:         `URLConvertible` value to be used as the `URLRequest`'s `URL`.
+    ///   - method:      `HTTPMethod` for the `URLRequest`.
+    ///   - parameters:  `Parameters` (a.k.a. `[String: Any]) value to be encoded into the `URLRequest`.
+    ///   - encoding:    `ParameterEncoding` to be used to encode the `parameters` value into the `URLRequest`.
+    ///   - headers:     `HTTPHeaders` value to be added to the `URLRequest`.
+    ///   - interceptor: `RequestInterceptor` value to be used by the resulting `DataRequest`.
+    /// - Returns: The created `DataRequest`.
     open func request(_ url: URLConvertible,
                       method: HTTPMethod = .get,
                       parameters: Parameters? = nil,
