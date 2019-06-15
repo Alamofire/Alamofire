0c7a91ad99289b1388447a43586c29eca63b75db
Jon Shier <jon@jonshier.com>
2019-06-15 15:39:13 -0400

    Whitespace cleanup.

--- Source/Protector.swift
+++ Source/Protector.swift
@@ -29,12 +29,12 @@ import Foundation
 /// An `os_unfair_lock` wrapper.
 final class UnfairLock {
     private let unfairLock: os_unfair_lock_t
-    
+
     init() {
         unfairLock = .allocate(capacity: 1)
         unfairLock.initialize(to: os_unfair_lock())
     }
-    
+
     deinit {
         unfairLock.deinitialize(count: 1)
         unfairLock.deallocate()
