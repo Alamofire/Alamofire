ce5be6fbc6f51414c49f56fc8e2b7c99253d9f8e
Jon Shier <jon@jonshier.com>
2019-09-03 22:36:28 -0400

    Add v5 package manifest.

--- Package@swift-4.2.swift
+++ Package@swift-4.2.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.2
 //
 //  Package.swift
 //
@@ -37,5 +37,5 @@ let package = Package(
             name: "Alamofire",
             path: "Source")
     ],
-    swiftLanguageVersions: [.v4, .v5]
+    swiftLanguageVersions: [.v3, .v4]
 )
