fba5829e6a41c8462f85206fe67710c18fcb2c74
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2019-10-20 13:14:57 +0200

    Rename attemptHTTPAuthentication

--- Source/SessionDelegate.swift
+++ Source/SessionDelegate.swift
@@ -80,7 +80,7 @@ extension SessionDelegate: URLSessionTaskDelegate {
         case NSURLAuthenticationMethodServerTrust:
             evaluation = attemptServerTrustAuthentication(with: challenge)
         case NSURLAuthenticationMethodHTTPBasic, NSURLAuthenticationMethodHTTPDigest, NSURLAuthenticationMethodNTLM, NSURLAuthenticationMethodNegotiate:
-            evaluation = attemptHTTPAuthentication(for: challenge, belongingTo: task)
+            evaluation = attemptCredentialAuthentication(for: challenge, belongingTo: task)
         // case NSURLAuthenticationMethodClientCertificate:
         // Alamofire doesn't currently support client certificate validation.
         default:
@@ -121,14 +121,14 @@ extension SessionDelegate: URLSessionTaskDelegate {
         }
     }
 
-    /// Evaluates the HTTP authentication `URLAuthenticationChallenge` received for `task`.
+    /// Evaluates the credential-based authentication `URLAuthenticationChallenge` received for `task`.
     ///
     /// - Parameters:
     ///   - challenge: The `URLAuthenticationChallenge`.
     ///   - task:      The `URLSessionTask` which received the challenge.
     ///
     /// - Returns:     The `ChallengeEvaluation`.
-    func attemptHTTPAuthentication(for challenge: URLAuthenticationChallenge,
+    func attemptCredentialAuthentication(for challenge: URLAuthenticationChallenge,
                                    belongingTo task: URLSessionTask) -> ChallengeEvaluation {
         guard challenge.previousFailureCount == 0 else {
             return (.rejectProtectionSpace, nil, nil)
