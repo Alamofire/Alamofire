2d673e49af5511a7304d447b3eed0195b7c9db2d
Jon Shier <jon@jonshier.com>
2020-05-24 22:45:45 -0400

    Add proper copyright header.

--- Tests/LeaksTests.swift
+++ Tests/LeaksTests.swift
@@ -1,9 +1,25 @@
 //
 //  LeaksTests.swift
-//  Alamofire
 //
-//  Created by Jon Shier on 5/21/20.
-//  Copyright © 2020 Alamofire. All rights reserved.
+//  Copyright (c) 2020 Alamofire Software Foundation (http://alamofire.org/)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import XCTest
@@ -38,12 +54,12 @@ final class LeaksTests: XCTestCase {
             }
             let process = leaksTo("/dev/null")
             guard process.terminationReason == .exit && [0, 1].contains(process.terminationStatus) else {
-                print("Weird, \(process.terminationReason): \(process.terminationStatus)")
+                print("Process terminated: \(process.terminationReason): \(process.terminationStatus)")
                 exit(255)
             }
             if process.terminationStatus == 1 {
                 print("================")
-                print("Oh no, we leaked")
+                print("Leaks Detected!!!")
                 leaksTo("/dev/tty")
             }
             exit(process.terminationStatus)
