b378d8697840e3d15febdf76f27275d059046c6e
park <pcugogo@gmail.com>
2020-12-29 22:42:53 +0900

    Fixed a memory leak issue.

--- Source/URLEncodedFormEncoder.swift
+++ Source/URLEncodedFormEncoder.swift
@@ -966,11 +966,11 @@ extension CharacterSet {
     /// In RFC 3986 - Section 3.4, it states that the "?" and "/" characters should not be escaped to allow
     /// query strings to include a URL. Therefore, all "reserved" characters with the exception of "?" and "/"
     /// should be percent-escaped in the query string.
-    public static let afURLQueryAllowed: CharacterSet = {
+    public static var afURLQueryAllowed: CharacterSet {
         let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
         let subDelimitersToEncode = "!$&'()*+,;="
         let encodableDelimiters = CharacterSet(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
 
         return CharacterSet.urlQueryAllowed.subtracting(encodableDelimiters)
-    }()
+    }
 }
