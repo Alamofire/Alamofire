9ae80814fa3c40213e88364cb6d12dd7c75d5109
Jon Shier <jon@jonshier.com>
2021-04-03 17:33:25 -0400

    Prepare 5.4.2 release.

--- Source/Alamofire.swift
+++ Source/Alamofire.swift
@@ -1,7 +1,7 @@
 //
 //  Alamofire.swift
 //
-//  Copyright (c) 2014-2018 Alamofire Software Foundation (http://alamofire.org/)
+//  Copyright (c) 2014-2021 Alamofire Software Foundation (http://alamofire.org/)
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -26,4 +26,4 @@
 public let AF = Session.default
 
 /// Current Alamofire version. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
-let version = "5.4.1"
+let version = "5.4.2"
