60593743ec88ce9788f8133fd93b4b35aa20ac86
Jon Shier <jon@jonshier.com>
2021-11-27 23:42:23 -0500

    Remove misleading Protected convenience APIs.

--- Source/Protected.swift
+++ Source/Protected.swift
@@ -136,46 +136,6 @@ final class Protected<T> {
     }
 }
 
-extension Protected where T: RangeReplaceableCollection {
-    /// Adds a new element to the end of this protected collection.
-    ///
-    /// - Parameter newElement: The `Element` to append.
-    func append(_ newElement: T.Element) {
-        write { (ward: inout T) in
-            ward.append(newElement)
-        }
-    }
-
-    /// Adds the elements of a sequence to the end of this protected collection.
-    ///
-    /// - Parameter newElements: The `Sequence` to append.
-    func append<S: Sequence>(contentsOf newElements: S) where S.Element == T.Element {
-        write { (ward: inout T) in
-            ward.append(contentsOf: newElements)
-        }
-    }
-
-    /// Add the elements of a collection to the end of the protected collection.
-    ///
-    /// - Parameter newElements: The `Collection` to append.
-    func append<C: Collection>(contentsOf newElements: C) where C.Element == T.Element {
-        write { (ward: inout T) in
-            ward.append(contentsOf: newElements)
-        }
-    }
-}
-
-extension Protected where T == Data? {
-    /// Adds the contents of a `Data` value to the end of the protected `Data`.
-    ///
-    /// - Parameter data: The `Data` to be appended.
-    func append(_ data: Data) {
-        write { (ward: inout T) in
-            ward?.append(data)
-        }
-    }
-}
-
 extension Protected where T == Request.MutableState {
     /// Attempts to transition to the passed `State`.
     ///
