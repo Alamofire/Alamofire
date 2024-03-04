4d67d4f7dd61377854848710d8cfa12972cc1a4b
Jon Shier <jon@jonshier.com>
2024-03-03 20:37:09 -0500

    Version to 5.9.0.

--- Source/Alamofire.swift
+++ Source/Alamofire.swift
@@ -39,5 +39,5 @@ public let AF = Session.default
 /// Namespace for informational Alamofire values.
 public enum AFInfo {
     /// Current Alamofire version.
-    public static let version = "5.8.1"
+    public static let version = "5.9.0"
 }
